8e166c1adef9c4636d9733bf5bb064bebec7d2d3
khodaparast <f.p.khodaparast@gmail.com>
2022-04-13 14:02:37 -0300

    Adding cepharmor module

---
+++ src/tools/cepharmor/KMS.cc
@@ -0,0 +1,38 @@
+
+#include "KMS.h"
+#include <string>
+
+using namespace std;
+
+
+
+class KeyHandler{
+  
+public:
+    int getAESSecret(char* pass, unsigned char* aeskey, unsigned char* aesiv){
+        EVP_MD_CTX *mdctx;
+        const EVP_MD *md;
+
+        unsigned char key[32];
+        unsigned char iv[16];
+        unsigned int md_len, i;
+
+        md = EVP_get_digestbyname("md5");
+
+        mdctx = EVP_MD_CTX_new();
+        EVP_DigestInit_ex(mdctx, md, NULL);
+        EVP_DigestUpdate(mdctx, pass, strlen(pass));
+
+        EVP_DigestFinal_ex(mdctx, key, &md_len);
+        EVP_MD_CTX_free(mdctx);
+
+        memcpy(iv, key, md_len/2);
+        memcpy(aeskey, key, md_len);
+        memcpy(aesiv, iv, md_len/2);
+
+   
+    return md_len;
+    }
+
+    
+};
