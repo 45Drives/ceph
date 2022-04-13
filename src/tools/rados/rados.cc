f73da37ce547c2a175069551978ddb67cdd0ad6f
khodaparast <f.p.khodaparast@gmail.com>
2022-04-13 17:01:33 -0300

    update

--- src/tools/rados/rados.cc
+++ src/tools/rados/rados.cc
@@ -12,11 +12,8 @@
  *
  */
 
-#include "include/types.h"
-
-#include "include/rados/buffer.h"
-#include "include/rados/librados.hpp"
-#include "include/rados/rados_types.hpp"
+#include "../CephArmor/Crypt.h"
+#include "rados.h"
 
 #include "acconfig.h"
 #ifdef WITH_LIBRADOSSTRIPER
@@ -24,44 +21,7 @@
  using namespace libradosstriper;
 #endif
 
-#include "common/config.h"
-#include "common/ceph_argparse.h"
-#include "global/global_init.h"
-#include "common/Cond.h"
-#include "common/debug.h"
-#include "common/errno.h"
-#include "common/Formatter.h"
-#include "common/obj_bencher.h"
-#include "common/TextTable.h"
-#include "include/stringify.h"
-#include "mds/inode_backtrace.h"
-#include "include/random.h"
-#include <iostream>
-#include <fstream>
 
-#include <stdlib.h>
-#include <time.h>
-#include <sstream>
-#include <errno.h>
-#include <dirent.h>
-#include <stdexcept>
-#include <climits>
-#include <locale>
-#include <memory>
-#include <optional>
-
-#include "cls/lock/cls_lock_client.h"
-#include "include/compat.h"
-#include "include/util.h"
-#include "common/hobject.h" 
-#include "../CephArmor/Crypt.h"
-#include "rados.h"
-
-
-#include "PoolDump.h"
-#include "RadosImport.h"
-
-#include "osd/ECUtil.h"
 
 using namespace librados;
 using ceph::util::generate_random_number;
